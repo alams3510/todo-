63f57241b4250e46239855d2b855b8f854f0e5e5
alams3510 <alams3510@gmail.com>
2022-01-05 00:18:07 +0530

    again updated search functionality

--- index.js
+++ index.js
@@ -9,26 +9,26 @@ let input = document.getElementById("input");
 
 addbtn.addEventListener('click', function () {
     let value = input.value;
-    if(value!=""){
-    let webtask = localStorage.getItem("key");
-    if (webtask == null) {
-        array = [];
+    if (value != "") {
+        let webtask = localStorage.getItem("key");
+        if (webtask == null) {
+            array = [];
+        }
+        else {
+            array = JSON.parse(webtask);
+        }
+        array.unshift(value);
+        localStorage.setItem("key", JSON.stringify(array))
     }
-    else {
-        array = JSON.parse(webtask);
-    }
-    array.push(value);
-    localStorage.setItem("key", JSON.stringify(array))
-    }
-     input.value="";
+    input.value = "";
     show();
 
 })
 // adding text on pressing on enter
 input.addEventListener('keyup', function (event) {
-    if(event.keyCode === 13){
-     event.preventDefault();
-     addbtn.click();
+    if (event.keyCode === 13) {
+        event.preventDefault();
+        addbtn.click();
     }
 
 })
@@ -60,60 +60,77 @@ function show() {
 
 // edit function start here
 
-function edittask(index){
+function edittask(index) {
     let webtask = localStorage.getItem("key");
-    let savebtn=document.getElementById("save");
-    let saveindex=document.getElementById("saveindex");
-    saveindex.value=index;
+    let savebtn = document.getElementById("save");
+    let saveindex = document.getElementById("saveindex");
+    saveindex.value = index;
     if (webtask == null) {
         array = [];
     }
     else {
         array = JSON.parse(webtask);
     }
-    input.value=array[index]
-    addbtn.style.display="none";
-    savebtn.style.display="inline";
+    input.value = array[index]
+    addbtn.style.display = "none";
+    savebtn.style.display = "inline";
 
 }
 
 // save function starts here
-let savebtn=document.getElementById("save");
+let savebtn = document.getElementById("save");
 
-    savebtn.addEventListener("click",function(){
-        let saveindex=document.getElementById("saveindex").value;
-        let webtask = localStorage.getItem("key");
-        array = JSON.parse(webtask);
-   array[saveindex]=input.value;
-   localStorage.setItem("key", JSON.stringify(array))
-   input.value="";
-   savebtn.style.display="none";
-   addbtn.style.display="inline";
-show();
-        
-    })
-
-    // delete function from here
-function deleteitem(index){
+savebtn.addEventListener("click", function () {
+    let saveindex = document.getElementById("saveindex").value;
     let webtask = localStorage.getItem("key");
     array = JSON.parse(webtask);
-    array.splice(index,1);
+    array[saveindex] = input.value;
+    localStorage.setItem("key", JSON.stringify(array))
+    input.value = "";
+    savebtn.style.display = "none";
+    addbtn.style.display = "inline";
+    show();
+
+})
+
+
+// delete function from here
+function deleteitem(index) {
+    let webtask = localStorage.getItem("key");
+    array = JSON.parse(webtask);
+    array.splice(index, 1);
     localStorage.setItem("key", JSON.stringify(array))
     show();
 }
 
 // delete all
 
-let delall=document.getElementById("del");
-delall.addEventListener("click",function(){
+let delall = document.getElementById("del");
+delall.addEventListener("click", function () {
     let webtask = localStorage.getItem("key");
     array = JSON.parse(webtask);
-    array=[];
+    array = [];
     localStorage.setItem("key", JSON.stringify(array))
-    savebtn.style.display="none";
-    addbtn.style.display="inline";
-show();
+    savebtn.style.display = "none";
+    addbtn.style.display = "inline";
+    show();
 })
 
 
+// searching function starts here
 
+function myinput() {
+    let search = document.getElementById("search").value.toUpperCase();
+    let mainlist = document.getElementById("list");
+    let list = mainlist.querySelectorAll("li");
+    console.log(search)
+
+ list.forEach((item,index) => {
+        let a = list[index].querySelectorAll("span")[1].innerText.toUpperCase();
+        if (a.indexOf(search)>-1) {
+             list[index].style.display = '';
+        } else {
+             list[index].style.display = "none";
+        }
+})
+}
